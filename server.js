3d6ef4b40ebcb792910efeb3991d8372bc456e51
Kim <cid.kim@gmail.com>
2023-12-20 05:07:43 -0700

    chore: install nodemon to run server on auto

--- server.js
+++ server.js
@@ -5,3 +5,7 @@ const PORT = 3333;
 
 api.use(express.json());
 api.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
+
+// Até aqui o código é só pegar o express, instanciar na memória, criar uma variável para a PORT e usar duas funções do express (dá pra ver essas funções listadas se tu der console.log(express))
+
+
