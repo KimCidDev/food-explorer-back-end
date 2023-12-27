0a03f75d31621b25532c827406a583ba8031914d
Kim <cid.kim@gmail.com>
2023-12-27 07:14:54 -0700

    build: unninstall express-async-error

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,6 +1,12 @@
+const AppError = require('../utils/AppError');
+
 class UserController {
   create(request, response) {
     const { name, email, password } = request.body;
+
+    if (!name) {
+      throw new AppError('Tu esqueceu do nome');
+    }
 
     return response.json({ name, email, password });
   }
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('express-async-errors');
 
 const express = require('express');
 const routes = require('./routes');
+const AppError = require('./utils/AppError');
 
 const database = require('./database/sqlite');
 
