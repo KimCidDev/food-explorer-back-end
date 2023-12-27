b32acd550e5c2829d8922a66f4eced13e031f13d
Kim <cid.kim@gmail.com>
2023-12-27 05:04:33 -0700

    feat: basic structure for noteController

---
+++ controllers/notesController.js
@@ -0,0 +1,9 @@
+class NotesController {
+  create(request, response) {
+    const { name, email, password } = request.body;
+
+    return response.json({ name, email, password });
+  }
+}
+
+module.exports = NotesController
