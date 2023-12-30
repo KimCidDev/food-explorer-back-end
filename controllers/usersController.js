52c890fed62d963e747086d17767893917ea5a15
Kim <cid.kim@gmail.com>
2023-12-30 07:27:36 -0700

    feat: create new user and register to sqlite db

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,14 +1,28 @@
 const AppError = require('../utils/AppError');
 
+const sqliteConnection = require('../database/sqlite');
+
 class UserController {
-  create(request, response) {
+  async create(request, response) {
     const { name, email, password } = request.body;
 
-    if (!name) {
-      throw new AppError('Tu esqueceu do nome');
+    const database = await sqliteConnection();
+    const isThereUser = await database.get('SELECT * FROM users WHERE (?)', [
+      email
+    ]);
+
+    if (isThereUser) {
+      throw new AppError(
+        'Esse usuário já está cadastrado. Utilize outro email'
+      );
     }
 
-    return response.json({ name, email, password });
+    await database.run(
+      'INSERT INTO users (name, email, password) VALUES (?, ?, ?)',
+      [name, email, password]
+    );
+
+    return response.status(201).json();
   }
 }
 
