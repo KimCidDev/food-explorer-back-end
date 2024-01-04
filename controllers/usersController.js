14f311b8b405224712a2ee38e0ed461189dbcafa
Kim <cid.kim@gmail.com>
2024-01-04 07:18:21 -0700

    build: knex install and config

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,8 +1,9 @@
-const { hash } = require('bcrypt');
+const { hash, compare } = require('bcrypt');
 
 const AppError = require('../utils/AppError');
 
 const sqliteConnection = require('../database/sqlite');
+const { param } = require('../routes/user.routes');
 
 class UserController {
   async create(request, response) {
@@ -27,6 +28,52 @@ class UserController {
     );
 
     return response.status(201).json();
+  }
+
+  async update(request, response) {
+    const { name, email, password, old_password } = request.body;
+    const { id } = request.params;
+
+    const database = await sqliteConnection();
+    const user = await database.get('SELECT * FROM users WHERE id = (?)', [id]);
+
+    if (!user) {
+      throw new AppError('Usuário não encontrado');
+    }
+
+    const userWithUpdatedEmail = await database.get(
+      'SELECT * FROM users WHERE email = (?)',
+      [email]
+    );
+
+    if (userWithUpdatedEmail && userWithUpdatedEmail.id !== id) {
+      throw new AppError('Este email já está e uso');
+    }
+
+    user.name = name;
+    user.email = email;
+
+    if (password && !old_password) {
+      throw new AppError('Tu precisa digitar a senha antiga');
+    }
+
+    if (password && old_password) {
+      const comparePasswords = await compare(password, old_password);
+
+      if (!comparePasswords) {
+        throw new AppError("A senha informada não está correta");
+      }
+    }
+
+    await database.run(
+      `
+      UPDATE users SET
+      name = ?,
+      email = ?,
+      updated_at = ?
+      WHERE id = (?)`,
+      [user.name, user.email]
+    );
   }
 }
 
