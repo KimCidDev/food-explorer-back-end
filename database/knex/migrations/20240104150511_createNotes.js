9b0e39a31f110ee400fc1fe24c36919827147b31
Kim <cid.kim@gmail.com>
2024-01-08 05:21:20 -0700

    feat: new migration for Notes

--- database/knex/migrations/20240104150511_createNotes.js
+++ database/knex/migrations/20240104150511_createNotes.js
@@ -1,2 +1,12 @@
-exports.up = function (knex) {};
-exports.down = () => {};
+exports.up = knex =>
+  knex.schema.createTable('notes', table => {
+    table.increments('id');
+    table.text('title');
+    table.text('descriptuon');
+    table.integer('user_id').references('id').inTable('users');
+
+    table.timestamp('created_at').default(knex.fn.now());
+    table.timestamp('updated_at').default(knex.fn.now());
+  });
+
+exports.down = knex => knex.schema.dropTable('notes');
