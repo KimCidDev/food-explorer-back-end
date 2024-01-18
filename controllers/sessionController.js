d09cb8fb4bbcf35fe0ffa3acbdfe1baab6f8fbaa
Kim <cid.kim@gmail.com>
2024-01-18 06:05:59 -0700

    feat: sessionController.create needs a password and check if password is correct

--- controllers/sessionController.js
+++ controllers/sessionController.js
@@ -1,5 +1,7 @@
 const AppError = require('../utils/AppError');
 
+const { compare } = require('bcrypt');
+
 const knex = require('../database/knex');
 
 class sessionController {
@@ -8,11 +10,22 @@ class sessionController {
 
     const user = await knex('users').where({ email }).first();
 
+    if (!password) {
+      throw new AppError('Preciso verificar tua senha, mermão', 401);
+    }
+
     if (!user) {
       throw new AppError(
         'te liga, mermão. Com esse email tu vai ter que te cadastrar primeiro',
-        201
+        401
       );
+    }
+
+    const passwordMatch = await compare(password, user.password);
+    console.log(passwordMatch);
+
+    if (!passwordMatch) {
+      throw new AppError('Perdão, a senha não é a mesma', 401);
     }
 
     return response.json(user);
