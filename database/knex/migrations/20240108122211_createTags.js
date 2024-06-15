ea723d9fbf374bd8d0b87342f83f2ca31f214776
Kim <cid.kim@gmail.com>
2024-06-15 10:01:55 -0600

    refactor[knex createTags migration]: improved query

--- database/knex/migrations/20240108122211_createTags.js
+++ database/knex/migrations/20240108122211_createTags.js
@@ -1,14 +1,21 @@
 exports.up = knex =>
   knex.schema.createTable('tags', table => {
     table.increments('id');
-    table.text('name').notNullable();
+    table.string('name', 100).notNullable(); // Assuming tag names are relatively short
 
     table
       .integer('dish_id')
+      .unsigned()
+      .notNullable()
       .references('id')
       .inTable('dishes')
       .onDelete('CASCADE');
-    table.integer('user_id').references('id').inTable('users');
+    table
+      .integer('user_id')
+      .unsigned()
+      .notNullable()
+      .references('id')
+      .inTable('users');
   });
 
 exports.down = knex => knex.schema.dropTable('tags');
