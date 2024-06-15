66cb608c72a9874232af0ac7ecdbf4dc48b4707d
Kim <cid.kim@gmail.com>
2024-06-15 09:59:31 -0600

    refactor[knex createDishmigration]: improving query

--- database/knex/migrations/20240104150511_createDish.js
+++ database/knex/migrations/20240104150511_createDish.js
@@ -1,16 +1,21 @@
 exports.up = knex =>
   knex.schema.createTable('dishes', table => {
     table.increments('id');
-    table.text('name');
-    table.text('description');
-    table.text('price');
-    table.text('category');
-    table.integer('quantity').defaultTo(1);
-    table.text('dishImg').defaultTo(null);
-    table.integer('user_id').references('id').inTable('users');
+    table.string('name', 255).notNullable();
+    table.text('description').notNullable();
+    table.decimal('price', 10, 2).notNullable(); // Assuming price is a decimal value
+    table.string('category', 100).notNullable();
+    table.integer('quantity').defaultTo(1).notNullable();
+    table.string('dishImg').defaultTo(null);
+    table
+      .integer('user_id')
+      .unsigned()
+      .references('id')
+      .inTable('users')
+      .onDelete('CASCADE');
 
-    table.timestamp('created_at').default(knex.fn.now());
-    table.timestamp('updated_at').default(knex.fn.now());
+    table.timestamp('created_at').defaultTo(knex.fn.now());
+    table.timestamp('updated_at').defaultTo(knex.fn.now());
   });
 
 exports.down = knex => knex.schema.dropTable('dishes');
