c346f4c54d6cfbff416961c6078cebd42a657986
Kim <cid.kim@gmail.com>
2024-06-17 04:19:12 -0600

    fix[knex table]: see if sqlite desappears

--- database/knex/migrations/20240615160953_create_users_table.js
+++ database/knex/migrations/20240615160953_create_users_table.js
@@ -1,12 +1,18 @@
-exports.up = knex =>
-  knex.schema.createTable('users', table => {
-    table.increments('id');
-    table.string('name', 50).notNullable();
-    table.string('email', 100).notNullable().unique();
-    table.string('password', 255).notNullable();
-    table.boolean('isAdmin').defaultTo(false);
-    table.timestamp('created_at').defaultTo(knex.fn.now());
-    table.timestamp('updated_at').defaultTo(knex.fn.now());
-  });
+exports.up = async function (knex) {
+  const exists = await knex.schema.hasTable('users');
+  if (!exists) {
+    return knex.schema.createTable('users', table => {
+      table.increments('id');
+      table.string('name', 50).notNullable();
+      table.string('email', 100).notNullable().unique();
+      table.string('password', 255).notNullable();
+      table.boolean('isAdmin').defaultTo(false);
+      table.timestamp('created_at').defaultTo(knex.fn.now());
+      table.timestamp('updated_at').defaultTo(knex.fn.now());
+    });
+  }
+};
 
-exports.down = knex => knex.schema.dropTable('users');
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists('users');
+};
